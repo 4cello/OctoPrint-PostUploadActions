ed1b7b35a931871bfd6dc89c5af43f7244d8474b
4cello <32751308+4cello@users.noreply.github.com>
2022-03-06 22:19:53 +0100

    added logic

--- octoprint_postuploadactions/__init__.py
+++ octoprint_postuploadactions/__init__.py
@@ -1,20 +1,31 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
+from threading import Timer
 
-class PostuploadactionsPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+class ConnectOnUploadPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.EventHandlerPlugin, octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin, octoprint.plugin.TemplatePlugin):
+    CONNECT_DELAY = 5.0
+    def on_after_startup(self):
+        helpers = self._plugin_manager.get_helpers("mqtt", "mqtt_publish")
+        if helpers:
+            if "mqtt_publish" in helpers:
+                self.mqtt_publish = helpers["mqtt_publish"]
+            try:			
+                self.mqtt_publish("octoprint/plugin/connectonupload/pub", "OctoPrint-ConnectOnUpload publishing.")
+            except:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(noMQTT=True))
+
+    def on_event(self, event, payload):
+        if event != "Upload":
+            return
+        if not payload["print"]:
+            return
+        self._logger.info(payload)
+        self._printer.connect(port="AUTO", baudrate="AUTO", profile="_default")
+        Timer(self.CONNECT_DELAY, self._printer.select_file, 
+            args=[payload["path"], False],kwargs=dict(printAfterSelect=True)).start()
 
     ##~~ SettingsPlugin mixin
 
@@ -60,8 +71,9 @@ class PostuploadactionsPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Postuploadactions Plugin"
-
+__plugin_name__ = "Connect on Upload"
+__plugin_version__ = "0.1.0"
+__plugin_description__ = "A quick \"Hello World\" example plugin for OctoPrint"
 
 # Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
 # OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
@@ -70,7 +82,7 @@ __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = PostuploadactionsPlugin()
+    __plugin_implementation__ = ConnectOnUploadPlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
